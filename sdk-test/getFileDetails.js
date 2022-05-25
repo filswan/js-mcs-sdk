3703a7288dc1a2f8b635671e6a780a08ced9a540
rykci <yuen.ricky@hotmail.com>
2022-05-25 13:39:45 -0400

    update import

--- sdk-test/getFileDetails.js
+++ sdk-test/getFileDetails.js
@@ -1,6 +1,6 @@
 require('dotenv').config('./.env')
 const fs = require('fs')
-const { mcsSdk } = require('../client/index')
+const { mcsSdk } = require('../sdk/index')
 const mcs = new mcsSdk({
   privateKey: process.env.PRIVATE_KEY,
   rpcUrl: process.env.RPC_URL,
@@ -9,8 +9,8 @@ const mcs = new mcsSdk({
 console.log(mcs.publicKey)
 
 async function main() {
-  const SOURCE_FILE_UPLOAD_ID = 1
-  const DEAL_ID = 1
+  const SOURCE_FILE_UPLOAD_ID = 0
+  const DEAL_ID = 0
 
   const mintResponse = await mcs.getFileDetails(SOURCE_FILE_UPLOAD_ID, DEAL_ID)
   console.log(mintResponse)
