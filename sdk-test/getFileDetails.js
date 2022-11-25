0e3eb0000777524aeb519fdaeda882352ada110f
rykci <yuen.ricky@hotmail.com>
2022-11-25 10:52:42 -0500

    mocha test script

--- sdk-test/getFileDetails.js
+++ sdk-test/getFileDetails.js
@@ -1,8 +1,6 @@
 require('dotenv').config('./.env')
 const { mcsSDK } = require('js-mcs-sdk')
 
-console.log(mcs.publicKey)
-
 async function main() {
   const SOURCE_FILE_UPLOAD_ID = 0
   const DEAL_ID = 0
