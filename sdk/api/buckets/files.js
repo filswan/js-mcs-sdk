98a8e96a7bc5974c1cf1b8855fa3e206324e528a
rykci <yuen.ricky@hotmail.com>
2022-12-08 12:43:38 -0500

    download files successfully

--- sdk/api/buckets/files.js
+++ sdk/api/buckets/files.js
@@ -1,8 +1,9 @@
-const { BUCKETS_API } = require('../../helper/constants')
-const { getBuckets } = require('./buckets')
-const axios = require('axios')
-const fs = require('fs')
-const { Agent } = require('https')
+const { BUCKETS_API } = require("../../helper/constants")
+const { getBuckets } = require("./buckets")
+const axios = require("axios")
+const fs = require("fs")
+const { Agent } = require("https")
+const { request } = require("urllib")
 
 const createUploadSession = async (jwt, bucketName, fileName, filePath) => {
   const bucketInfo = await getBuckets(jwt, bucketName)
@@ -20,7 +21,7 @@ const createUploadSession = async (jwt, bucketName, fileName, filePath) => {
   try {
     const res = await axios.put(`${BUCKETS_API}file/upload`, sessionObj, config)
 
-    if (res.data.status === 'error') {
+    if (res.data.status === "error") {
       throw new Error(res.data.message)
     }
 
@@ -44,18 +45,18 @@ const uploadToBucket = async (jwt, bucketName, fileName, filePath) => {
       jwt,
       bucketName,
       fileName,
-      filePath,
+      filePath
     )
 
-    if (session.status == 'error') throw new Error(session.message)
+    if (session.status == "error") throw new Error(session.message)
 
     const res = await axios.post(
       `${BUCKETS_API}file/upload/${session.data.sessionID}/0`,
       fs.createReadStream(filePath),
-      config,
+      config
     )
 
-    if (res.data.status === 'error') {
+    if (res.data.status === "error") {
       throw new Error(res.data.message)
     }
 
@@ -65,4 +66,30 @@ const uploadToBucket = async (jwt, bucketName, fileName, filePath) => {
   }
 }
 
-module.exports = { uploadToBucket }
+const downloadFile = async (jwt, bucketName, fileName, outputDirectory) => {
+  try {
+    let bucketInfo = await getBuckets(jwt, bucketName)
+    let files = bucketInfo.data?.objects
+
+    let file = files.find((f) => f.name == fileName)
+
+    if (!file) {
+      throw new Error("file not found")
+    }
+
+    let name = outputDirectory.endsWith("/")
+      ? outputDirectory + file.ipfs_url?.split("?filename=").pop()
+      : outputDirectory + "/" + file.ipfs_url?.split("?filename=").pop()
+
+    let res = await request(file.ipfs_url)
+    await fs.promises.writeFile(name, res.data, (err) => {
+      if (err) {
+        console.error(err)
+      }
+    })
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+module.exports = { uploadToBucket, downloadFile }
