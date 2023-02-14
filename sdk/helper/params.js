90d9a1bea341b98f05f8a6c16979f6f7c6de0090
rykci <yuen.ricky@hotmail.com>
2023-02-14 10:21:45 -0500

    add jwt

--- sdk/helper/params.js
+++ sdk/helper/params.js
@@ -1,7 +1,11 @@
 const axios = require('axios')
-const getParams = async (api) => {
+const getParams = async (api, jwt) => {
+  const config = {
+    headers: { Authorization: `Bearer ${jwt}` },
+  }
+
   try {
-    const params = await axios.get(`${api}/v1/common/system/params`)
+    const params = await axios.get(`${api}/v1/common/system/params`, config)
     return params.data?.data
   } catch (err) {
     console.log(err)
