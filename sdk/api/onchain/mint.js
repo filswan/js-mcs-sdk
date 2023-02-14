6cee985a8c04366ccbcebe4fda13cd0da0580129
rykci <yuen.ricky@hotmail.com>
2023-02-14 11:32:53 -0500

    folder restructure

--- sdk/api/onchain/mint.js
+++ sdk/api/onchain/mint.js
@@ -1,7 +1,7 @@
-const factoryABI = require('../abi/CollectionFactory.json')
+const factoryABI = require('./abi/CollectionFactory.json')
 const axios = require('axios')
 const { mcsUpload } = require('./upload')
-const { getParams } = require('../helper/params')
+const { getParams } = require('../../utils/params')
 
 const createCollection = async (api, jwt, web3, payer, collectionJson) => {
   const uploadResponse = await mcsUpload(
@@ -13,7 +13,7 @@ const createCollection = async (api, jwt, web3, payer, collectionJson) => {
 
   let collection_uri = uploadResponse.pop().data.ipfs_url
 
-  const params = await getParams(api)
+  const params = await getParams(api, jwt)
   const factoryAddress = params.nft_collection_factory_address
   const factoryContract = new web3.eth.Contract(factoryABI, factoryAddress)
 
@@ -105,7 +105,7 @@ const mint = async (
 
   let nft_uri = uploadResponse.pop().data.ipfs_url
 
-  const params = await getParams(api)
+  const params = await getParams(api, jwt)
   const factoryAddress = params.nft_collection_factory_address
   const factoryContract = new web3.eth.Contract(factoryABI, factoryAddress)
 
